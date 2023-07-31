efc49d263b52be08b3e765e3ef291b7e0afbb0c5
RayhaanK <rayhaankhan331@gmail.com>
2023-07-31 14:25:56 +0200

    added readfile and append

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const {writeFile, readFile, appendFile, read} = require('fs')
 
-readFile('./data.txt', 'utf-8', (err, data)=>{
-    if(!err) 
-        console.log(data);
-    else
+// First content:
+// const content = "Hi, my name is Rayhaan";
+
+const content = "Hi, my name is no.";
+
+writeFile('./data.txt', content, err=> {
+    try {
+        (!err) 
+        console.log(content)
+    }
+      catch {
+          console.log("An error occured");
+      }
+      
+});
+
+const content2 = " New data is inserted here"
+
+appendFile('./data.txt', content2, err => {
+    try {
+        (!err)
+        console.log('Data was appended!');
+    }
+    catch {
+        console.log('An error occured');
+    }
+  });
+
+
+readFile('./data.txt', 'utf-8', (err, content)=>{
+    try {
+        (!err) 
+        console.log(content);
+    }
+    catch {
         console.log("An error occured");
+    }
 })
 
-// The second content was placed in the next line
+// The content was overwritting by the second content I added on the same line.
