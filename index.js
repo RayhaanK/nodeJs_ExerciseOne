046501c9d69592dbd7191a6c73767865f45835dd
RayhaanK <rayhaankhan331@gmail.com>
2023-07-31 14:28:55 +0200

    moved answer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {writeFile, readFile, appendFile, read} = require('fs')
+const {writeFile, readFile, appendFile} = require('fs')
 
 // First content:
 // const content = "Hi, my name is Rayhaan";
@@ -10,10 +10,10 @@ writeFile('./data.txt', content, err=> {
         (!err) 
         console.log(content)
     }
-      catch {
-          console.log("An error occured");
-      }
-      
+    catch {
+        console.log("An error occured");
+    }
+    
 });
 
 const content2 = " New data is inserted here"
@@ -26,7 +26,8 @@ appendFile('./data.txt', content2, err => {
     catch {
         console.log('An error occured');
     }
-  });
+});
+// The content was overwritting by the second content I added on the same line.
 
 
 readFile('./data.txt', 'utf-8', (err, content)=>{
@@ -38,5 +39,3 @@ readFile('./data.txt', 'utf-8', (err, content)=>{
         console.log("An error occured");
     }
 })
-
-// The content was overwritting by the second content I added on the same line.
